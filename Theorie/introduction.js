1cd57a175790ce6a36490add2724e03487b130ff
Jordy Murgueitio <jordinho.15@gmail.com>
2023-02-12 12:37:22 -0500

    adding theorie file

---
+++ Theorie/introduction.js
@@ -0,0 +1,25 @@
+//PROPERTIES - data types have properties
+
+console.log("hello".length); //length propertie - prints 5
+
+//METHODS - actions we can perform
+
+console.log("hello".toUpperCase()); //prints HELLO
+console.log("mija".startsWith("m")); //prints true
+
+
+
+// BUILT-IN OBJECTS - objects have methods and properties
+
+console.log("hola"); //console object - log method - prints hola
+
+Math.random(); //Math object with random method 
+console.log(Math.random()); //prints random number between 0 and 1
+
+Math.floor(Math.random() * 50); //floor method rounds down the nearest whole number
+console.log(Math.floor(Math.random() * 50)); //prints whole random number between 0 and 50
+
+Number.isInteger(2030); //Number object with isInteger method
+console.log(Number.isInteger(120)); //prints true
+
+
