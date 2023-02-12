419a6d15b34e6a6e5342f7506aebd848d4999cf2
Jordy Murgueitio <jordinho.15@gmail.com>
2023-02-12 11:57:00 -0500

    adding some practice with basic js

--- practice/index.js
+++ practice/index.js
@@ -1,5 +1,11 @@
-document.getElementById("demo").innerHTML = "Hola";
+/* PRACTICE GETTING HTML ELEMENTS */
+
+/* document.getElementById("demo").innerHTML = "Hola";
 
 document.getElementById("demo").style.color = "blue";
 
-document.querySelector(".title").style.color = "blue";
+document.querySelector(".title").style.color = "blue"; */
+
+/* PRACTICE */
+console.log("hola " + "Mija");
+console.log("Hello".startsWith("m"));
