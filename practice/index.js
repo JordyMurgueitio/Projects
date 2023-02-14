1ba36a4c6911275613da855078ff0a9d1e5d8aff
Jordy Murgueitio <jordinho.15@gmail.com>
2023-02-14 11:17:13 -0500

    adding js for form

--- practice/index.js
+++ practice/index.js
@@ -9,3 +9,18 @@ document.querySelector(".title").style.color = "blue"; */
 /* PRACTICE */
 console.log("hola " + "Mija");
 console.log("Hello".startsWith("m"));
+
+/* 
+51744d62-7800-43f2-a396-5b48ec8bbb8d */
+
+function emailSend () {
+    Email.send({
+        SecureToken : "51744d62-7800-43f2-a396-5b48ec8bbb8d",
+        To : 'jordinho.15@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New message",
+        Body : document.getElementById("name") + "age" + document.getElementById("age") + document.getElementById("email")
+    }).then(
+        message => alert(message)
+    );
+}
