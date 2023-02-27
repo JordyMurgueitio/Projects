d3cb31c72af8d37c47ba90d5b70fb4266160060f
Jordy Murgueitio <jordinho.15@gmail.com>
2023-02-26 23:54:49 -0500

    finished simple part color flipper

--- color-flipper/logic.js
+++ color-flipper/logic.js
@@ -1,3 +1,21 @@
-const colors = ["blue", "red", "green", "yellow"];
+// generating color options
+const colors = ["rgb(226, 12, 12)", "rgb(21, 36, 172)", "rgb(35, 131, 26)", "rgb(214, 211, 7)"];
+// getting button and saving it in a variable
 const btn = document.getElementById("btn");
+// getting the color text
 const color = document.querySelector(".color");
+
+
+// adding event to button
+btn.addEventListener("click", function(){ 
+    // saves the random number 
+    const randomNumber = getRandomNumber(); 
+    // colors[randomNumber] picks one of the color options randomly when the btn is clicked
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.innerHTML = colors[randomNumber];
+})
+
+// generates a random number between 0 and colors.length
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
