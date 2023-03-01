61ff77614577f296ed707738d7fdb305093cbbe3
Jordy Murgueitio <jordinho.15@gmail.com>
2023-02-28 22:32:48 -0500

    adding tutoring file

---
+++ Salah/logic.js
@@ -0,0 +1,28 @@
+let number = 5; 
+let number2 = 5;
+
+// alert(number % number2)
+
+// && : logical and
+
+
+// if(number > number2){
+//     console.log("number 1 is greater");
+// } 
+// else if(number == number2){
+//     console.log("number 2 is eaqual to number 1");
+// }
+// else {
+//     console.log("number 2 is greater");
+// }
+
+// for(let i=0; i<20; i++){
+//     console.log('me gusta el espagnol '+i)
+// }
+
+function my_addition(a, b){
+    let result = a+b
+    alert('the result of the addition is : '+result)
+}
+
+my_addition(6, 2);
