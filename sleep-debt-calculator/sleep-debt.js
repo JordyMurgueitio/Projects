6a67d58ee5a1137887e773151053e5ac23681dca
Jordy Murgueitio <jordinho.15@gmail.com>
2023-03-07 14:31:47 -0500

    improving sleep debt calculator

--- sleep-debt-calculator/sleep-debt.js
+++ sleep-debt-calculator/sleep-debt.js
@@ -1,43 +1,54 @@
-// get the sleep my sleep hours per day
+// SLEEP-DEBT-CALCULATOR 
 
-    function getSleepHours(day) {
-    if (day === "monday") {
-        return 8;
-        } else if (day=== "tuesday") {
-        return 8;
-        } else if (day === "wednesday") {
-        return 8;
-        } else if (day === "thrusday") {
-        return 7; 
-        } else if (day === "friday") {
-        return 8; 
-        }
+//get the sleep hours per day
+function getSleepHours (day) {
+    switch (day) {
+        case "monday":
+            return 8;
+        break;
+
+        case "tuesday":
+            return 7;
+        break;
+
+        case "wednesday":
+            return 2;
+        break;
+
+        case "thursday":
+            return 5;
+        break;
+
+        case "friday":
+            return 8;
+        break;
+    } 
 }
 
-    // Calculating actual sleep hours for the week. 
-    function getActualSleepHours() {
-    return getSleepHours("monday") + getSleepHours("tuesday") + getSleepHours("wednesday") + getSleepHours("thrusday") + getSleepHours("friday");
-    }
-    console.log(`Actual sleep hours: ${getActualSleepHours()}`)
 
-    //Getting ideal sleep hours 
-    const getIdealSleepHours = () => {
-        let idealHours = 8; 
-        return idealHours * 5; 
+// Calculating actual sleep hours for the week. 
+const getActualSleepHours = () => getSleepHours("monday") + getSleepHours("tuesday") + getSleepHours("wednesday") +  getSleepHours("thursday") + getSleepHours("friday");
+
+
+//Getting ideal sleep hours per night and multiplying for 5 (days of the week)
+const getIdealSleepHours = (idealHours) => idealHours * 5;
+
+
+//Calculating sleep debt. 
+function calculateSleepDebt () {
+    let totalHours = getActualSleepHours();
+    let idealSleepHours = getIdealSleepHours(9); 
+
+    if (totalHours > idealSleepHours) {
+        return  `You have slept ${totalHours} hours this week, the ideal is ${idealSleepHours} hours. You are over the ideal for ${totalHours - idealSleepHours} hours`;
+    } else if (totalHours < idealSleepHours) {
+        return `You have slept ${totalHours} hours this week, the ideal is ${idealSleepHours} hours. You are under the ideal for ${idealSleepHours - totalHours} hours`;
+    } else {
+        return `You have slept ${totalHours} hours this week, the ideal is ${idealSleepHours} hours. You are fine`;
     }
-    console.log(`ideal sleep hours: ${getIdealSleepHours()}`)
-    
-    //Calculating sleep debt. 
-    
-    const calculateSleepDebt = () => {
-        let actualSleepHours = getActualSleepHours(); 
-        let idealSleepHours = getIdealSleepHours(); 
-        if (actualSleepHours === idealSleepHours) {
-        console.log("Perfect amount of sleep"); 
-        } else if (actualSleepHours > idealSleepHours) {
-        console.log("You are fuckin lazy"); 
-        } else {
-        console.log("you need more sleep");
-        }
-    };
-    calculateSleepDebt();
+}
+
+console.log(calculateSleepDebt());  
+
+
+
