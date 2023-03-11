572a74dd1d5b815eec2235074db2b788e3701548
Jordy Murgueitio <jordinho.15@gmail.com>
2023-03-11 17:05:40 -0500

    adding project

--- training-days/traning.js
+++ training-days/traning.js
@@ -1,7 +1,11 @@
 // TRAINING DAYS. 
 
-// 
+/* program prints to the console the name of the user 
+with the event that the user signed up for and the days left to train  */
 
+
+
+// 1. Function generates a random event with 3 options
 const getRandEvent = () => {
     const random = Math.floor(Math.random() * 3);
     if (random === 0) {
@@ -12,8 +16,11 @@ const getRandEvent = () => {
         return 'Pentathlon';
     }
 };
+const event = getRandEvent();   // saves the event in a variable
+const event2 = getRandEvent();
 
 
+// 2. function return the number of days based on the event 
 const getTrainingDays = event => {
     let days = 0;
     if (event === 'Marathon') {
@@ -25,24 +32,25 @@ const getTrainingDays = event => {
     }
     return days;
 };
+const days = getTrainingDays(event); 
+const days2 = getTrainingDays(event2);
 
+// 3. Save the user name in a variable
+const name = "Nala";
+const name2 = 'Warren';
 
+// 4. function prints the name of the user and the event the signed up for
 const logEvent = (name, event) => {
     console.log(`${name}'s event is: ${event}`);
 };
 
+// 5. function prints the name of the user and the days left
 const logTime = (name, days) => {
     console.log(`${name}'s time to train is: ${days} days`);
 };
 
-const event = getRandEvent();
-const days = getTrainingDays(event);
-const name = "Nala";
 logEvent(name, event);
 logTime(name, days);
 
-const event2 = getRandEvent();
-const days2 = getTrainingDays(event2);
-const name2 = 'Warren';
 logEvent(name2, event2);
 logTime(name2, days2);
